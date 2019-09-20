58d242b8cebdaf65c949d368fbe1a92f487285eb
Jessica Rendón Buitrago <JessicaRendon@Jessicas-MacBook-Pro.local>
2019-09-20 08:46:35 -0500

    :c

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 var messagesArray = document.querySelectorAll ('.message');
 
+
+//fncion que obtiene el texto escrito en el input
+function handleWrite() {
+    if (e.keyCode == 13) {
+        var leText = document.getElementsByClassName('.write');
+    }
+    leText.appendChild('.message');
+}
+message.addEventListener( );
+
+
 function handleMessageEnter(){
     var message = message.getAttribute('data-text');
-   
+    
     var container = document.createElement('div');
     container.classList.add('message__container');
     container.innerHTML = `
-        <p>${text}</p>
+    <p>${text}</p>
     `;
     message.appendChild(container);
 }
@@ -14,10 +25,3 @@ function handleMessageEnter(){
 //aqupi falta algooo
 
 
-//fncion que obtiene el texto escrito en el input
-function lemessage() {
-    if (e.keyCode == 13) {
-        var leText = document.getElementsByClassName('.write');
-leText.appendChild();
-    }
-}
